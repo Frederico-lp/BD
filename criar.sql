4f78d51f7e70de2dd13ff751252ea01cacd41e71
Frederico Lopes <frederico.m.lps@gmail.com>
2021-12-04 23:44:44 +0000

    first commit

---
+++ criar.sql
@@ -0,0 +1,13 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
+
+
+-- DROP TABLE IF EXISTS ();
+-- CREATE TABLE  ();
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = ON;
---
+++ povoar.sql
@@ -0,0 +1,12 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+PRAGMA foreign_keys = ON;
+BEGIN TRANSACTION;
+
+-- INSERT INTO () VALUES ();
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = ON;
