07a9b127e0125d033c1c9cfdff72a94ca3aaa544
Nuno Miguel Carvalho de Jesus <up201905477@fe.up.pt>
2021-12-06 00:43:13 +0000

    Fixed a few bugs, added a lot of 'NOT NULL', changed and added some constraints and organized the code.

--- criar.sql
+++ criar.sql
@@ -3,141 +3,134 @@
 .nullvalue NULL
 
 PRAGMA foreign_keys=ON;
---BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Notification;
+DROP TABLE IF EXISTS Subscription;
+DROP TABLE IF EXISTS CreditCard;
+DROP TABLE IF EXISTS Show;
+DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Serie;
+DROP TABLE IF EXISTS Season;
+DROP TABLE IF EXISTS Episode;
+DROP TABLE IF EXISTS Genre;
+DROP TABLE IF EXISTS Following;
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Watched;
+DROP TABLE IF EXISTS ShowGenre;
+
 CREATE TABLE User(
     id INT PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     birth DATE,
-    fullName TEXT,
-    subActibe BOOL,
+    fullName TEXT NOT NULL,
+    subActive BOOL NOT NULL,
     id_Subscription INT REFERENCES Subscription
 );
 
-DROP TABLE IF EXISTS Notification;
 CREATE TABLE Notification(
     id INT PRIMARY KEY,
     content TEXT NOT NULL,
     id_User INT REFERENCES User
-
 );
 
-DROP TABLE IF EXISTS Subscription;
 CREATE TABLE Subscription(
     id INT PRIMARY KEY,
-    initDate DATE,
-    endDate DATE,
-    id_CreditCard INT REFERENCES CreditCard
+    initDate DATE NOT NULL,
+    endDate DATE NOT NULL,
+    id_CreditCard INT NOT NULL UNIQUE REFERENCES CreditCard,
 
-    CONSTRAINT dates CHECK (initDate < endDate)
+    CONSTRAINT validDates CHECK (initDate < endDate),
+    CONSTRAINT numMonthsSubscribed CHECK (endDate - initDate IN (1, 6, 12)) -- Não está a funcionar
 );
 
-DROP TABLE IF EXISTS CreditCard;
 CREATE TABLE CreditCard(
     id INT PRIMARY KEY,
-    cardNumber INT(16,0),    --<= 8 digits
-    securityNumber INT(4,0),
-    name TEXT NOT NULL,
-    expireDate DATE
+    cardNumber INT(16,0) NOT NULL UNIQUE,    --<= 8 digits
+    securityNumber INT(4,0) NOT NULL,
+    name TEXT NOT NULL UNIQUE,
+    expireDate DATE NOT NULL,
 
-    CONSTRAINT cardNumberSize CHECK (cardNumber > 999999999999999)
-    CONSTRAINT securityNumberSize CHECK (securityNumber > 999)
-    CONSTRAINT expiringDate CHECK (expireDate > DATE('now'))
+    CONSTRAINT cardNumberSize CHECK (cardNumber > 999999999999999),
+    CONSTRAINT securityNumberSize CHECK (securityNumber > 999),
+    CONSTRAINT expiringDate CHECK (expireDate > CURRENT_DATE)
 );
 
-DROP TABLE IF EXISTS Show;
 CREATE TABLE Show(
     id INT PRIMARY KEY,
     name TEXT NOT NULL,
-    launchDate DATE,
-    synopsis TEXT,
-    rating INT,
+    launchDate DATE NOT NULL,
+    synopsis TEXT NOT NULL,
+    rating FLOAT NOT NULL,
 
-    CONSTRAINT ratings CHECK (rating >= 0 AND rating <= 5)
+    CONSTRAINT ratings CHECK (rating >= 0.0 AND rating <= 10.0)
 );
 
-DROP TABLE IF EXISTS Movie;
 CREATE TABLE Movie(
     id INT PRIMARY KEY REFERENCES Show,
-    duration TIME,
+    duration TIME NOT NULL,
+
+    CONSTRAINT validDuration CHECK (duration != '00:00:00')
 );
 
-DROP TABLE IF EXISTS Serie;
 CREATE TABLE Serie(
     id INT PRIMARY KEY REFERENCES Show
 );
 
-DROP TABLE IF EXISTS Season;
 CREATE TABLE Season(
     id INT PRIMARY KEY,
-    number INT,
-    id_Serie INT REFERENCES Serie
+    number INT NOT NULL,
+    id_Serie INT REFERENCES Serie,
 
     CONSTRAINT seasonNumber CHECK (number > 0)
 );
 
-DROP TABLE IF EXISTS Episode;
 CREATE TABLE Episode(
     id INT PRIMARY KEY,
-    number INT,
+    number INT NOT NULL,
     name TEXT NOT NULL,
-    duration TIME,
-    launchDate DATE,
-    id_Season INT REFERENCES Season
+    duration TIME NOT NULL,
+    launchDate DATE NOT NULL,
+    id_Season INT REFERENCES Season,
+
+    CONSTRAINT validDuration CHECK (duration != '00:00:00')
 );
 
-DROP TABLE IF EXISTS Genre;
 CREATE TABLE Genre(
     id INT PRIMARY KEY,
-    name TEXT
+    name TEXT NOT NULL UNIQUE
 );
 
-
-DROP TABLE IF EXISTS Following;
 CREATE TABLE Following(
     id_Serie INT REFERENCES Serie,
     id_User INT REFERENCES User,
 
-    PRIMARY KEY(id_Serie, id_User),
+    PRIMARY KEY(id_Serie, id_User)
 );
 
-DROP TABLE IF EXISTS Comment;
 CREATE TABLE Comment(
     id_Episode INT REFERENCES Episode,
     id_User INT REFERENCES User,
-    cDate DATE DEFAULT DATE('now'),
+    cDate DATE NOT NULL,
     content TEXT NOT NULL,
 
-    PRIMARY KEY(id_Serie, id_User),  
+    PRIMARY KEY(id_Episode, id_User)
 );
 
-DROP TABLE IF EXISTS Watched;
 CREATE TABLE Watched(
     id_Episode INT REFERENCES Episode,
     id_User INT REFERENCES User,
-    wdate DATE DEFAULT DATE('now'),
+    wdate DATE NOT NULL,
 
-    PRIMARY KEY(id_Serie, id_User),
+    PRIMARY KEY(id_Episode, id_User)
 );
 
-DROP TABLE IF EXISTS ShowGenre;
 CREATE TABLE ShowGenre(
     id_Show INT REFERENCES Show,
     id_Genre INT REFERENCES Genre,
 
-    PRIMARY KEY(id_Serie, id_User),    
+    PRIMARY KEY(id_Show, id_Genre)
 );
 
-
-
-
-
-
-
-
-
---COMMIT TRANSACTION;
---PRAGMA foreign_keys = ON;
--- povoar.sql
+++ povoar.sql
@@ -2,11 +2,6 @@
 .headers on
 .nullvalue NULL
 
-
 PRAGMA foreign_keys = ON;
-BEGIN TRANSACTION;
 
 -- INSERT INTO () VALUES ();
-
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = ON;
