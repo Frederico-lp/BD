8e6f96878d1e668eb52fad1a055a3d3928b09189
Tomás Agante Martins <50155550+tagantemartins@users.noreply.github.com>
2022-01-26 20:11:23 +0000

    Update gatilho3_adiciona.sql

--- triggers/gatilho3_adiciona.sql
+++ triggers/gatilho3_adiciona.sql
@@ -4,4 +4,9 @@
 
 PRAGMA foreign_keys = ON;
 
-
+CREATE TRIGGER IF NOT EXISTS GATILHO_3
+BEFORE INSERT ON ShowGenre
+WHEN NEW.id_Genre = NULL
+BEGIN
+  SELECT raise(abort, 'INVALID INSERTION: the show must have at least 1 genre.');
+END;
