092d26edad1d34eb42bcb033b61bcbb6a141a221
Tomás Agante Martins <50155550+tagantemartins@users.noreply.github.com>
2022-01-26 18:30:15 +0000

    Update int7.sql

--- queries/int7.sql
+++ queries/int7.sql
@@ -4,5 +4,10 @@
 
 PRAGMA foreign_keys = ON;
 
--- SELECT
+-- listar os comentários que 
+-- foram publicados antes de um episódio ser lançado
 
+SELECT name, content, cDate, launchDate
+FROM Comment, Episode
+WHERE Comment.id_Episode = Episode.id
+AND DATE(Comment.cDate) < DATE(Episode.launchDate);
