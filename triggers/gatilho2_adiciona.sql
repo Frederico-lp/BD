4bb936e99d3d0b700340cb89271443c46821361c
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-01-26 03:14:34 +0000

    Initiated trigger 2.

--- triggers/gatilho2_adiciona.sql
+++ triggers/gatilho2_adiciona.sql
@@ -4,4 +4,16 @@
 
 PRAGMA foreign_keys = ON;
 
+-- A data de lançamento de um episódio ou filme tem que ser inferior à data 
+-- em que esse conteúdo é visualizado pelos utilizadores, 
+-- que têm que ter subscrição ativa para ver esse mesmo conteúdo.
 
+CREATE TRIGGER IF NOT EXISTS GATILHO_2
+BEFORE INSERT ON Watched
+WHEN NOT EXISTS (
+  SELECT * 
+  FROM User 
+  WHERE User.id = NEW.id_User AND subActive = true)
+BEGIN
+  SELECT raise(abort, 'Invalid insert: a user must have an active subscription and can not watch episodes yet to launch.');
+END;
