a59c66cde8d73edb5e398ea0e10ebf0389380718
Tomás Agante Martins <50155550+tagantemartins@users.noreply.github.com>
2022-01-26 20:34:02 +0000

    Update gatilho1_adiciona.sql

--- triggers/gatilho1_adiciona.sql
+++ triggers/gatilho1_adiciona.sql
@@ -4,25 +4,12 @@
 
 PRAGMA foreign_keys = ON;
 
---notificate user when new episode of serie he is following is added
-CREATE TRIGGER IF NOT EXISTS GATILHO_1
-AFTER INSERT
-ON Episode
---quando user seguir serie
--- WHEN
---     SELECT count(id_User) > 0
---     FROM Following, (
---         SELECT id_Serie as id_of_serie
---         FROM Season
---         WHERE id_Serie = (
---             SELECT id_Serie 
---             FROM Season
---             WHERE id = NEW.id_Season
---             --WHERE id = NEW.id_Season
---             )
---     )
---     WHERE id_Serie = id_of_serie
+-- Notificar o utilizador quando novo episódio for lançado. 
+-- (NOTA: é utilizado o auto increment no id da tabela Notification 
+-- para inserir uma nova notificação, de modo a não inserir o id manualmente).
 
+CREATE TRIGGER IF NOT EXISTS GATILHO_1
+AFTER INSERT ON Episode
 
 BEGIN
     INSERT INTO Notification
@@ -38,6 +25,3 @@ BEGIN
     )
     WHERE id_Serie = id_of_serie;
 END;
-
-
-
